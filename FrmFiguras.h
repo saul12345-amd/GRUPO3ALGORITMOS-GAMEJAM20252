4cf542ca36b6bfbd5e8bf337d5fffaf3ec939aee
gp0l <misagelgianpool18@gmail.com>
2025-11-09 03:01:54 -0500

    Terminado pro max

--- FrmFiguras.h
+++ FrmFiguras.h
@@ -6,7 +6,6 @@
 #include "Rombo.h"
 #include "FrmMoverFiguraTecla.h"
 
-
 namespace Semana10 {
 
 	using namespace System;
@@ -23,13 +22,14 @@ namespace Semana10 {
 		{
 			InitializeComponent();
 
-			// Al iniciar, ocultar los botones
+			// Al iniciar, ocultar los botones y el segundo fondo
 			btnAutomatico->Visible = false;
 			btnJugador->Visible = false;
+			pictureBox2->Visible = false;
 
 			// Configurar y arrancar el Timer
 			timer1 = gcnew Timer();
-			timer1->Interval = 3000; // 3 segundos
+			timer1->Interval = 2000; // 2 segundos
 			timer1->Tick += gcnew EventHandler(this, &FrmFiguras::timer1_Tick);
 			timer1->Start();
 		}
@@ -45,11 +45,12 @@ namespace Semana10 {
 
 	private:
 		System::Windows::Forms::PictureBox^ pictureBox1;
+		System::Windows::Forms::PictureBox^ pictureBox2;
 		System::Windows::Forms::Button^ btnAutomatico;
 		System::Windows::Forms::Button^ btnJugador;
 		System::Windows::Forms::Timer^ timer1;
-
-		System::ComponentModel::Container^ components;
+	private: System::Windows::Forms::PictureBox^ pictureBox3;
+		   System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		void InitializeComponent(void)
@@ -58,7 +59,11 @@ namespace Semana10 {
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->btnAutomatico = (gcnew System::Windows::Forms::Button());
 			this->btnJugador = (gcnew System::Windows::Forms::Button());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// pictureBox1
@@ -76,20 +81,20 @@ namespace Semana10 {
 			this->btnAutomatico->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->btnAutomatico->Font = (gcnew System::Drawing::Font(L"Microsoft Tai Le", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->btnAutomatico->Location = System::Drawing::Point(130, 304);
+			this->btnAutomatico->Location = System::Drawing::Point(172, 353);
 			this->btnAutomatico->Name = L"btnAutomatico";
 			this->btnAutomatico->Size = System::Drawing::Size(246, 82);
 			this->btnAutomatico->TabIndex = 1;
-			this->btnAutomatico->Text = L"AUTOMATICO";
+			this->btnAutomatico->Text = L"AUTOMÁTICO";
 			this->btnAutomatico->UseVisualStyleBackColor = false;
-			this->btnAutomatico->Click += gcnew System::EventHandler(this, &FrmFiguras::button1_Click);
+			this->btnAutomatico->Click += gcnew System::EventHandler(this, &FrmFiguras::btnAutomatico_Click);
 			// 
 			// btnJugador
 			// 
 			this->btnJugador->BackColor = System::Drawing::Color::Lime;
 			this->btnJugador->Font = (gcnew System::Drawing::Font(L"Microsoft Tai Le", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->btnJugador->Location = System::Drawing::Point(457, 304);
+			this->btnJugador->Location = System::Drawing::Point(476, 353);
 			this->btnJugador->Name = L"btnJugador";
 			this->btnJugador->Size = System::Drawing::Size(246, 82);
 			this->btnJugador->TabIndex = 2;
@@ -97,46 +102,88 @@ namespace Semana10 {
 			this->btnJugador->UseVisualStyleBackColor = false;
 			this->btnJugador->Click += gcnew System::EventHandler(this, &FrmFiguras::btnJugador_Click);
 			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(-66, -1);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(1052, 625);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox2->TabIndex = 3;
+			this->pictureBox2->TabStop = false;
+			this->pictureBox2->Visible = false;
+			this->pictureBox2->Click += gcnew System::EventHandler(this, &FrmFiguras::pictureBox2_Click);
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.Image")));
+			this->pictureBox3->Location = System::Drawing::Point(289, 103);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(362, 220);
+			this->pictureBox3->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox3->TabIndex = 4;
+			this->pictureBox3->TabStop = false;
+			this->pictureBox3->Click += gcnew System::EventHandler(this, &FrmFiguras::pictureBox3_Click);
+			// 
 			// FrmFiguras
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(928, 621);
+			this->Controls->Add(this->pictureBox2);
 			this->Controls->Add(this->btnJugador);
 			this->Controls->Add(this->btnAutomatico);
 			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->pictureBox3);
 			this->Name = L"FrmFiguras";
 			this->Text = L"Interfaz de Inicio";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 
+		// ===============================
+		//        EVENTOS
+		// ===============================
+
 	private:
-		// Evento: clic en botón automático
-		System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Evento del temporizador
+		System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+
+			pictureBox3->Visible = false;
+			timer1->Stop();
+
+			// Ocultar la primera imagen
+			pictureBox1->Visible = false;
+
+			// Mostrar la segunda imagen y botones
+			pictureBox2->Visible = true;
+			pictureBox3->Visible = true;
+			pictureBox2->BringToFront();
+			pictureBox3->BringToFront();
+
+			btnAutomatico->Visible = true;
+			btnJugador->Visible = true;
+			btnAutomatico->BringToFront();
+			btnJugador->BringToFront();
+		}
+
+		// Evento: clic en botón Automático
+		System::Void btnAutomatico_Click(System::Object^ sender, System::EventArgs^ e) {
 			MessageBox::Show("Modo Automático seleccionado");
 		}
 
-		// Evento: temporizador
-		System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
-			// Detener el temporizador
-			timer1->Stop();
-
-			// Ocultar la imagen (desaparece el fondo)
-			pictureBox1->Visible = false;
-
-			// Mostrar los botones
-			btnAutomatico->Visible = true;
-			btnJugador->Visible = true;
-
-			// Cambiar color de fondo si deseas (opcional)
-			this->BackColor = System::Drawing::Color::Black;
+		// Evento: clic en botón Jugador
+		System::Void btnJugador_Click(System::Object^ sender, System::EventArgs^ e) {
+			FrmMoverFiguraTecla^ formulario = gcnew FrmMoverFiguraTecla();
+			formulario->Show();
 		}
-	private: System::Void btnJugador_Click(System::Object^ sender, System::EventArgs^ e) {
-		FrmMoverFiguraTecla^ formulario = gcnew FrmMoverFiguraTecla();
-		formulario->Show();
+	private: System::Void pictureBox2_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+private: System::Void pictureBox3_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
